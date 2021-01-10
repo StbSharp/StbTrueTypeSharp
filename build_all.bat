2adb321ce72acb0a3118e85dc007808bb5f4638c
Roman Shapiro <roman.shapiro@grapecity.com>
2021-01-10 08:23:14 +0700

    Changed target frameworks to "netstandard2.0;net45"

--- build_all.bat
+++
@@ -1,5 +0,0 @@
-dotnet --version
-dotnet build StbTrueTypeSharp.sln /p:Configuration=Release --no-incremental
-call copy_zip_package_files.bat
-rename "ZipPackage" "StbTrueTypeSharp.%APPVEYOR_BUILD_VERSION%"
-7z a StbTrueTypeSharp.%APPVEYOR_BUILD_VERSION%.zip StbTrueTypeSharp.%APPVEYOR_BUILD_VERSION%
